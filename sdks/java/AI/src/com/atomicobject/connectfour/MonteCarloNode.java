0898e07e30829b8d821303b6dadd27a105800142
Jacob Rich <charles.jacob.rich@gmail.com>
2020-11-14 16:48:34 -0500

    added getvisits and ucb to montecarlonode

--- sdks/java/AI/src/com/atomicobject/connectfour/MonteCarloNode.java
+++ sdks/java/AI/src/com/atomicobject/connectfour/MonteCarloNode.java
@@ -5,6 +5,29 @@ import java.util.List;
 public class MonteCarloNode {
 	private MonteCarloNode parent;
 	private List<MonteCarloNode> children;
+	private double temperature;
 	private int totalValue;
 	private int visits;
+	private int zobristKey;
+
+	public MonteCarloNode(MonteCarloNode parent, double temperature) {
+		this.parent = parent;
+		this.temperature = temperature;
+		totalValue = 0;
+		visits = 0;
+	}
+
+	public int getVisits() {
+		return visits;
+	}
+
+	public double upperConfidenceBound() {
+		double ucb;
+		double avgValue = totalValue / (double) visits;
+
+		ucb = avgValue + (temperature * Math.sqrt(Math.log(parent.getVisits()) / visits));
+
+		return ucb;
+	}
+
 }
